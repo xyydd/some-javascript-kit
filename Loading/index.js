a25ecd9a2ac038efda29ba1120ee2c56753c6bd3
xyydd <453539707@qq.com>
2024-11-21 15:23:48 +0800

    feat:add vue2 antdv loading directive

--- Loading/index.js
+++ Loading/index.js
@@ -1,8 +1,10 @@
-import Loading from './AdntvLoading.vue';
+import Vue from 'vue';
+import Loading from './AdntvLoading';
 import packageInfo from './package.json';
-const install = (Vue) =>  {
-  Vue.prototype.$Loading = (message) => {
-    const LoadingConstructor = Vue.extend(Loading);
+
+const LoadingConstructor = Vue.extend(Loading);
+const install = (Vue) => {
+  Vue.prototype.$Loading = (message, loadingTime = 0) => {
     LoadingConstructor.prototype.close = function () {
       if (this.$el && this.$el.parentNode) {
         this.$el.parentNode.removeChild(this.$el);
@@ -16,15 +18,59 @@ const install = (Vue) =>  {
       },
     });
     document.body.appendChild(instance.$el);
+    if (!isNaN(loadingTime) && loadingTime > 0) {
+      setTimeout(() => {
+        Vue.prototype.$Loading.close();
+      }, loadingTime);
+    }
     return instance;
   };
-  // Vue.directive('loading', {
-  //   bind(el, binding) {
-  //     if (binding.value) {}
-  //   }
-  // })
+  Vue.directive('loading', {
+    bind(el, binding) {
+      if (binding.value) {
+        const instance = new LoadingConstructor({
+          el: document.createElement('div'),
+        });
+        console.log(el, el.parentNode);
+        el.appendChild(instance.$el);
+        el.instance = {
+          close: () => {
+            el.removeChild(instance.$el);
+          },
+        };
+      } else {
+        if (el.instance) {
+          el.instance.close();
+        }
+      }
+    },
+    update(el, binding) {
+      if (binding.value !== binding.oldValue) {
+        if (binding.value) {
+          const instance = new LoadingConstructor({
+            el: document.createElement('div'),
+          });
+          el.appendChild(instance.$el);
+          el.instance = {
+            close: () => {
+              el.removeChild(instance.$el);
+            },
+          };
+        } else {
+          if (el.instance) {
+            el.instance.close();
+          }
+        }
+      }
+    },
+    unbind(el) {
+      if (el.instance) {
+        el.instance.close();
+      }
+    },
+  });
 };
 export default {
   version: packageInfo.version,
-  install: install
+  install: install,
 };
